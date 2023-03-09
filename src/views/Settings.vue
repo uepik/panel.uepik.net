f388757acf80627c6ac71745b9fc71c4f7e0dee1
Kacper Stawiński <stawinskidev@gmail.com>
2023-03-09 17:35:23 +0100

    feat: create `Settings` view

---
+++ src/views/Settings.vue
@@ -0,0 +1,27 @@
+<script setup>
+import DashboardOverlay from '@/components/DashboardOverlay.vue'
+</script>
+
+<template>
+  <DashboardOverlay>
+    <v-row>
+      <v-col md="6" sm="12">
+        <v-card>
+          <v-card-title>Ustawienia konta</v-card-title>
+          <v-card-text>
+            <pre>{{ $store.getters.user }}</pre>
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+      <v-col md="6" sm="12">
+        <v-card>
+          <v-card-title>Ustawienia firmy</v-card-title>
+          <v-card-text>
+            <pre>{{ $store.getters.user.company }}</pre>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </DashboardOverlay>
+</template>
