000d8999d0f663ddef0103c2d989405e2e938fc9
Kacper Stawiński <kacper.stawinski@frc.org.pl>
2023-05-06 11:18:39 +0200

    fix(table): smaller bottom margin for summaries

--- src/components/FinancesSummaryBox.vue
+++ src/components/FinancesSummaryBox.vue
@@ -5,7 +5,7 @@ const props = defineProps(['title', 'value'])
 <template>
   <v-col md="4" sm="12">
     <v-card class="mb-4">
-      <v-card-title>{{ title }}</v-card-title>
+      <v-card-title class="mb-0">{{ title }}</v-card-title>
       <v-card-text class="text-h6 font-weight-regular">{{ value }}</v-card-text>
     </v-card>
   </v-col>
